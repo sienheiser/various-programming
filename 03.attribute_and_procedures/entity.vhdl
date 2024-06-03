58a930706cba514b932642e4824ae2c30796677a
emmanueldcosta <emmanuel.dcosta@ess.eu>
2024-06-03 08:00:00 +0200

    Defined entity

---
+++ 03.attribute_and_procedures/entity.vhdl
@@ -0,0 +1,26 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use work.pkg.all;
+
+entity ent is
+    port(
+        clk : in std_logic;
+        inp : in std_logic_vector(2 downto 0);
+        enc : out opl_enc_t
+    );
+end entity ent;
+
+architecture behav of ent is
+begin
+    process(clk) is
+    begin
+        if rising_edge(clk) then
+            case inp is
+                when "001" =>
+                    enc <= S_OK;
+                when others =>
+                    enc <= S_EBI;
+            end case;
+        end if;
+    end process;
+end architecture behav;
