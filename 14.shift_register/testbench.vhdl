21e3e57cb4e79e9fada721f3e3b68fcd4f77cac5
emmanueldcosta <emmanuel.dcosta@ess.eu>
2024-06-19 14:48:07 +0200

    Added test for behavioural shift_register

--- 14.shift_register/testbench.vhdl
+++ 14.shift_register/testbench.vhdl
@@ -8,6 +8,7 @@ architecture behave of testbench is
     signal clk_tb: std_logic := '0';
     signal d_tb: std_logic := '0';
     signal q_tb: std_logic;
+    signal q1_tb:std_logic;
     component shift_register is 
         port(
             clk: in std_logic;
@@ -15,13 +16,26 @@ architecture behave of testbench is
             q_f: out std_logic
         );
     end component shift_register;
+    component sr is 
+        port(
+            i_clk: in std_logic;
+            i_d: in std_logic;
+            o_q: out std_logic
+        );
+    end component sr;
 begin
-    sr: shift_register
+    sr0: shift_register
         port map (
             clk => clk_tb,
             d0 => d_tb,
             q_f => q_tb
         );    
+    sr1: sr
+        port map(
+            i_clk => clk_tb,
+            i_d => d_tb,
+            o_q => q1_tb
+        );
     clock_process:process is 
     begin
         clk_tb <= '1';
