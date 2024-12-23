dee5308d0a3b07ec1719dd65ff524f78825914bf
sienheiser <emmanueldcosta97@gmail.com>
2024-12-23 11:45:00 +0100

    Continued solving problems

--- TeachYourselfCS/SICP/Chapter2/Chapter2-3.scm
+++ TeachYourselfCS/SICP/Chapter2/Chapter2-3.scm
@@ -190,3 +190,29 @@
         ((= x (entry set) set))
         ((> x (entry set) (make-tree (car set) (left-branch set) (adjoin-set x (right-branch set)))))
         ((< x (entry set) (make-tree (car set) (adjoin-set x (left-branch set)) (right-branch set))))))
+
+;Exercise 2.64
+(define (list->tree elements)
+  (car (partial-tree elements (length elements))))
+
+(define (partial-tree elts n)
+  (if (= n 0)
+    (cons '() elts)
+    (let ((left-size (quotient (- n 1) 2)))
+      (let ((left-result 
+              (partial-tree elts left-size)))
+        (let ((left-tree (car left-result)) 
+              (non-left-elts (cdr left-result)) 
+              (right-size (- n (+ left-size 1))))
+          (let ((this-entry (car non-left-elts)) 
+                (right-result 
+                  (partial-tree 
+                    (cdr non-left-elts) 
+                    right-size)))
+            (let ((right-tree (car right-result))
+                  (remaining-elts
+                    (cdr right-result)))
+              (cons (make-tree this-entry
+                               left-tree
+                               right-tree)
+                    remaining-elts))))))))
