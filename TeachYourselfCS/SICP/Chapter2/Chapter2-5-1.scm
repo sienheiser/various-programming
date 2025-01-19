9a549da64aecc1ded36ad8457e26617ae6e42592
sienheiser <emmanueldcosta97@gmail.com>
2025-01-19 11:05:22 +0100

    Continued solving exercises

---
+++ TeachYourselfCS/SICP/Chapter2/Chapter2-5-1.scm
@@ -0,0 +1,117 @@
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+        (apply proc (map contents args))
+        (error "No method for these types: APPLY-GENERIC" (list op type-tags))))))
+
+(define (attach-tag type-tag contents)
+  (cons type-tag contents))
+
+;Generic arithmetic operations
+(define (add x y) (apply-generic 'add x y))
+(define (sub x y) (apply-generic 'sub x y))
+(define (mul x y) (apply-generic 'mul x y))
+(define (div x y) (apply-generic 'div x y))
+
+
+;defining packages
+;;Scheme numbers
+(define (intsall-scheme-number-package)
+  (define (tag x) (attach-tag 'scheme-number x))
+  (put 'add '(scheme-number scheme-number) 
+       (lambda (x y) (tag (+ x y))))
+  (put 'sub '(scheme-number scheme-number) 
+       (lambda (x y) (tag (- x y))))
+  (put 'mul '(scheme-number scheme-number) 
+       (lambda (x y) (tag (* x y))))
+  (put 'div '(scheme-number scheme-number) 
+       (lambda (x y) (tag (/ x y))))
+  (put 'make 'scheme-number (lambda (x) (tag x)))
+  'done)
+
+(define (make-scheme-number x)
+  ((get 'make 'scheme-number) x))
+
+
+;;Rational numbers
+(define (install-rational-package)
+  ;; internal procedures
+  (define (numer x) (car x))
+  (define (denom x) (cdr x))
+  (define (make-rat n d)
+    (let ((g (gcd n d)))
+      (cons (/ n g) (/ d g))))
+  (define (add-rat x y)
+    (make-rat (+ (* (numer x) (denom y))
+                 (* (numer y) (denom x)))
+              (* (denom x) (denom y))))
+  (define (sub-rat x y)
+    (make-rat (- (* (numer x) (denom y))
+                 (* (numer y) (denom x)))
+              (* (denom x) (denom y))))
+  (define (mul-rat x y)
+    (make-rat (* (numer x) (numer y))
+              (* (denom x) (denom y))))
+  (define (div-rat x y)
+    (make-rat (* (numer x) (denom y))
+              (* (denom x) (numer y))))
+  (define (tag x) (attach-tag 'rational x))
+  ;; interface to rest of the system
+  (put 'add '(rational rational)
+       (lambda (x y) (tag (add-rat x y))))
+  (put 'sub '(rational rational)
+       (lambda (x y) (tag (sub-rat x y))))
+  (put 'mul '(rational rational)
+       (lambda (x y) (tag (mul-rat x y))))
+  (put 'div '(rational rational)
+       (lambda (x y) (tag (div-rat x y))))
+  (put 'make 'rational
+       (lambda (n d) (tag (make-rat n d))))
+  'done)
+(define (make-rational-number n d)
+  ((get 'make 'rational) n d))
+
+;;Complex numbers
+(load "Chapter2-4-data-directed.scm")
+(define (install-complex-package)
+  ;;imported functions
+  (define (make-from-real-imag x y)
+    ((get 'make-from-real-imag 'rectangular) x y))
+  (define (make-from-mag-ang r a)
+    ((get 'make-from-mag-ang 'polar) r a))
+
+  ;; internal procedures
+  (define (add-complex z1 z2)
+    (make-from-real-imag (+ (real-part z1) (real-part z2))
+                         (+ (imag-part z1) (imag-part z2)))
+  (define (sub-complex z1 z2)
+    (make-from-real-imag (- (real-part z1) (real-part z2))
+                         (- (imag-part z1) (imag-part z2)))
+  (define (mul-complex z1 z2)
+    (make-from-mag-ang (* (magnitude z1) (magnitude z2))
+                       (+ (angle z1) (angle z2))))
+  (define (div-complex z1 z2)
+    (make-from-mag-ang (/ (magnitude z1) (magnitude z2))
+                       (- (angle z1) (angle z2))))
+  
+  ;;interfacing to rest of system
+  (define (tag x) (atach-tag 'complex x))
+
+  (put 'add '(complex complex)
+       (lambda (z1 z2) (tag (add-complex z1 z2))))
+  (put 'sub '(complex complex)
+       (lambda (z1 z2) (tag (sub-complex z1 z2))))
+  (put 'mul '(complex complex)
+       (lambda (z1 z2) (tag mul-complex z1 z2))))
+  (put 'div '(complex complex)
+       (lambda (z1 z2) (tag (div-complex z1 z2))))
+
+  (put 'make-from-real-imag 'complex
+       (lambda (x y) (make-from-real-imag x y)))
+  (put 'make-from-mag-ang 'complex
+       (lambda (r a) (make-from-mag-ang r a))):w
+
+
+
+
