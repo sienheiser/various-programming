6aa5e4d4074c337f553d4b820617ff6133a5476f
sienheiser <emmanueldcosta97@gmail.com>
2025-03-28 07:25:22 +0100

    Continued solving exercises

--- TeachYourselfCS/SICP/Chapter3/Chapter3-3-2.scm
+++ TeachYourselfCS/SICP/Chapter3/Chapter3-3-2.scm
@@ -1,51 +1,39 @@
 (require racket/mpair)
 ;queue
+(define (set-car! x item) (set-mcar! x item))
+(define (set-cdr! x item) (set-mcdr! x item))
+
 ; A queue is a pair that point to the front and end of a list.
-(define (set-cdr! q item)
-    (set-mcdr! q item))
-(define (set-car! q item)
-    (set-mcar! q item))
+(define (make-queue) (mcons null null))
 
-;; Conctructor
-(define (make-queue)
-    (mcons null null))
+(define (front-ptr queue) (mcar queue))
+(define (rear-ptr queue) (mcdr queue))
 
-;; getters
-(define (front-ptr q)
-    (mcar q))
-(define (rear-ptr q)
-    (mcdr q))
+(define (empty-queue? queue) (eq? (front-ptr queue) null))
 
-;; setters
-(define (set-front-ptr! q item)
-    (set-car! q item))
-(define (set-rear-ptr! q item)
-    (set-cdr! q item))
+(define (set-front-ptr! queue item)(set-car! queue item))
+(define (set-rear-ptr! queue item) (set-cdr! queue item))
 
-;; types of queues
-(define (empty-queue? q)
-    (null? (front-ptr q)))
-
-;; update queue methods
-(define (delete-queue! q)
-    (cond ((empty-queue? q)
-           (error "DELETE! called with an empty queue" q))
-           (else (set-front-ptr! q (mcdr (front-ptr q)))
-                 q)))
-
-
-(define (insert-queue! q item)
+(define (insert-queue queue item)
     (let ((new-pair (mcons item null)))
-        (cond ((empty-queue? q)
-            (set-front-ptr! q new-pair)
-            (set-rear-ptr! q new-pair)
-            q)
-            (else (set-cdr! (rear-ptr q) new-pair)
-                  (set-rear-ptr! q new-pair)
-                  q))))
+        (cond ((empty-queue? queue) 
+               (set-front-ptr! queue new-pair)
+               (set-rear-ptr! queue new-pair)
+               queue)
+               (else
+                (set-cdr! (rear-ptr queue) new-pair)
+                (set-rear-ptr! queue new-pair)
+                queue))))
+
+(define (delete-queue queue)
+    (cond ((empty-queue? queue)
+           (error "Tried delete-queue on empty queue" queue))
+           (else 
+            (set-front-ptr! queue (mcdr (front-ptr queue)))
+            queue)))
 
 
-;Exercise 3.22 Make queue representation for printing
+;Exercise 2.21 Make queue representation for printing
 ;Why does the standard print out not work?
 ;The example given in exercise 3.22 shows an issue with the standard
 ;printing method. When you insert two items and then delete the queue,
@@ -55,37 +43,71 @@
         (print "(mcons '() '())")
         (print queue)))
 
+;Exercise 2.22 Build queue as a procedure with local state. The procedure will 
+;have form
+(define (make-queue)
+    (let ((front-ptr null)
+          (rear-ptr null))
+        
+        (define (empty-queue?)(eq? front-ptr null))
 
+        (define (get-front-ptr) front-ptr)
+
+        (define (get-rear-ptr) rear-ptr)
+
+        (define (set-front-ptr! new-val)
+            (set! front-ptr new-val)
+            )
+
+        (define (set-rear-ptr! new-val)
+            (set! rear-ptr new-val)
+            )
+
+        (define (delete!)
+            (cond ((empty-queue?) (error "Nothing to delete in empty queue"))
+                  (else (set-front-ptr! (mcdr front-ptr))
+                  front-ptr)))
+
+        (define (insert! item)
+            (let ((new-pair (mcons item null)))
+                (cond ((empty-queue?)
+                       (set-front-ptr! new-pair)
+                       (set-rear-ptr! new-pair)
+                       front-ptr)
+                      (else
+                       (set-cdr! rear-ptr new-pair)
+                       (set-rear-ptr! new-pair)
+                       front-ptr)
+                       )))
+
+        (define (dispatch m)
+            (cond ((eq? m 'get-front-ptr) (get-front-ptr))
+                  ((eq? m 'get-rear-ptr) (get-rear-ptr))
+                  ((eq? m 'set-front-ptr!) set-front-ptr!)
+                  ((eq? m 'set-rear-ptr!) set-rear-ptr!)
+                  ((eq? m 'delete!) (delete!))
+                  ((eq? m 'insert!) insert!)
+                  (else (error "Undefined action for message" m))))
+        dispatch))
+
+(define x (make-queue))
+(define y (mlist 1 2 3))
 
 ;Exercise 3.23 Make deque
-
-(define (make-deque) (cons null null))
-
-(define (empty-deque? dq) 
-    (eq? (car dq) null))
-
-(define (front-deque dq)
-    (car dq))
-
-(define (rear-deque dq)
-    (cdr dq))
-
-(define (rear-insert-deque! dq item)
-    (let (new-pair (mcons item null))
-        (cond ((empty-deque? dq)
-            (set-front-ptr! dq new-pair)
-            (set-rear-ptr! dq new-pair)
-            dq)
-            (else (set-cdr! (rear-ptr dq) new-pair)
-                  (set-rear-ptr! dq new-pair)
-                  dq))))
-
-(define (front-insert-deque! dq item)
-    (let (new-ls (mcons item (front-ptr dq)))
-        (cond ((empty-deque? dq)
-               (set-front-ptr! dq new-ls)
-               (set-rear-ptr! dq new-ls)
-               dq)
-              ((else 
-                (set-front-ptr! dq new-ls)
-                dq)))))
+(define (make-deque)
+    (let ((front-ptr null)
+          (rear-ptr null))
+        (define (set-front-ptr! new-val)
+            (set! front-ptr new-val))
+        (define (set-rear-ptr! new-val)
+            (set! rear-ptr new-val))
+        (define (delete-front!)
+            (cond ((empty-queue?) (error "Nothing to delete in empty deque"))
+                  (else (set-front-ptr! (mcdr front-ptr))
+                  front-ptr)))
+        (define (delete-rear!)
+            (cond ((empty-queue?) (error "Nothing to delte in empty deque"))
+                  (else (set-rear-ptr ...))))          
+        (define (insert-front! new-val))
+        (define (insert-rear! new-val))
+        ))
