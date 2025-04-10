1078ef628e589774cbb48140d4fa4b50ba5cc129
sienheiser <emmanueldcosta97@gmail.com>
2025-04-10 14:51:33 +0200

    Continued working throught the chapter

--- TeachYourselfCS/SICP/Chapter3/Chapter3-3-4.scm
+++ TeachYourselfCS/SICP/Chapter3/Chapter3-3-4.scm
@@ -1,3 +1,4 @@
+(require racket/mpair)
 ;Primitive function boxes
 
 (define (get-signal wire))
@@ -84,7 +85,7 @@
               c-out
               c))
 
-(deifne (ripple-carry-adder as bs ss init-c-in)
+(define (ripple-carry-adder as bs ss init-c-in)
     (define init-c-out (make-wire))
     (define init-c-out2 (make-wire))
     (define (iterator as bs c-in ss c-out)
@@ -122,4 +123,57 @@
 
 
 
-              
+;Representing a wire
+(define (make-wire)
+    (let ((signal-value 0)
+          (action-procedures '()))
+
+         (define (set-my-signal! new-value)
+            (if (not (= signal-value new-value))
+                (begin
+                    (set! signal-value new-value)
+                    (call-each action-procedures))
+                'done))
+
+         (define (accept-action-procedure! proc)
+            (set! action-procedures
+                  (cons proc action-procedures))
+            (proc))
+
+         (define (dispatch m)
+            (cond ((eq? m 'get-signal) signal-value)
+                  ((eq? m 'set-my-signal!) set-my-signal!)
+                  ((eq? m 'add-action!) accept-action-procedure!)
+                  (else (error "Unkown operation: WIRE" m))))
+         dispatch))
+
+(define (call-each procedures)
+    (if (null? procedures)
+        'done
+        (begin ((mcar procedures))
+               (call-each (mcdr procedures)))))
+
+(define (get-signal wire) (wire 'get-signal))
+(define (set-signal! wire value) ((wire 'set-signal) value))
+(define (add-action! wire action-procedure) ((wire 'add-action) action-procedure))
+(define (after-delay delay action)
+    (add-to-aggenda! (+ delay (current-time the-agenda))
+                     action
+                     the-agenda))
+
+(define (propagate)
+    (if (empty-agenda? the-agenda)
+         'done
+         (let ((first-item (first-agenda-item the-agenda)))
+              (first-item)
+              (remove-first-agenda-item! the-agenda)
+              (propagate))))
+
+(define (probe name wire)
+    (add-action! wire
+                 (lambda ()
+                    (newline)
+                    (display name) (display " ")
+                    (display (current-time the-agenda))
+                    (display " New-value = ")
+                    (display (get-signal wire)))))
