16f6cc70324d6d2522ed941a325774417c6ac202
sienheiser <emmanueldcosta97@gmail.com>
2025-04-17 08:16:11 +0200

    Continued playing around with the system

--- TeachYourselfCS/SICP/Chapter3/testbench.scm
+++ TeachYourselfCS/SICP/Chapter3/testbench.scm
@@ -12,21 +12,40 @@
                                   (get-value a2))))))
     (define (dispatch m)
         (cond ((eq? m 'process-value) process-value)
-              (else (error "Unkown case: ADDER" m)))))
+              (else (error "Unkown case: ADDER" m))))
+    dispatch)
+
+(define (process-value operator)
+    ((operator 'process-value)))
           
+(define (has-value? connector)
+    ((connector 'has-value?)))
+
+(define (set-value! connector value)
+    ((connector 'set-value!) value))
+
+(define (get-value connector)
+    ((connector 'get-value)))
+
 (define (make-connector)
     (let ((value false))
          (define (set-value! new-value)
             (set! value new-value))
          (define (has-value?)
             (if value
-                value
+                true
                 false))
+         (define (get-value) value)
          (define (dispatch m)
             (cond ((eq? m 'has-value?) has-value?)
                   ((eq? m 'set-value!) set-value!)
+                  ((eq? m 'get-value) get-value)
                   (else (error "Unkown request: CONNECTOR" m))))
          dispatch))
 
-(define (has-value? connector)
-    (connector 'has-value?))
+
+(define a1 (make-connector))
+(define a2 (make-connector))
+(define sum (make-connector))
+
+(define ad (adder a1 a2 sum))
