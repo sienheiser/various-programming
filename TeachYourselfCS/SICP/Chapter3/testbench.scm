b2339182c60733720399f4d883a7093a74117db2
sienheiser <emmanueldcosta97@gmail.com>
2025-05-27 07:38:52 +0200

    Made working implementation of stream

--- TeachYourselfCS/SICP/Chapter3/testbench.scm
+++ TeachYourselfCS/SICP/Chapter3/testbench.scm
@@ -1,51 +1,23 @@
+(define (stream-enumerate-interval low high)
+    (if (> low high)
+        empty-stream
+        (cons-stream low
+                     (stream-enumerate-interval (+ 1 low) high))))
 
-(define (adder a1 a2 sum)
-    (define (process-value)
-        (cond ((and (has-value? a1) (has-value? a2))
-               (set-value! sum (+ (get-value a1)
-                                  (get-value a2))))
-              ((and (has-value? sum) (has-value? a1))
-               (set-value! sum (- (get-value sum)
-                                  (get-value a1))))
-              ((and (has-value? sum) (has-value? a2))
-               (set-value! sum (- (get-value sum)
-                                  (get-value a2))))))
-    (define (dispatch m)
-        (cond ((eq? m 'process-value) process-value)
-              (else (error "Unkown case: ADDER" m))))
-    dispatch)
+(define (cons-stream a b)
+    (cons a (delay b)))
 
-(define (process-value operator)
-    ((operator 'process-value)))
-          
-(define (has-value? connector)
-    ((connector 'has-value?)))
+(define (delay exp)
+    (lambda () (exp)))
 
-(define (set-value! connector value)
-    ((connector 'set-value!) value))
+(define (stream-car s)
+    (car s))
 
-(define (get-value connector)
-    ((connector 'get-value)))
+(define (stream-cdr s)
+    (force (cdr s)))
 
-(define (make-connector)
-    (let ((value false))
-         (define (set-value! new-value)
-            (set! value new-value))
-         (define (has-value?)
-            (if value
-                true
-                false))
-         (define (get-value) value)
-         (define (dispatch m)
-            (cond ((eq? m 'has-value?) has-value?)
-                  ((eq? m 'set-value!) set-value!)
-                  ((eq? m 'get-value) get-value)
-                  (else (error "Unkown request: CONNECTOR" m))))
-         dispatch))
+(define (force exp)
+    (exp))
 
-
-(define a1 (make-connector))
-(define a2 (make-connector))
-(define sum (make-connector))
-
-(define ad (adder a1 a2 sum))
+(define (stream-empty? s)
+    (eq? empty-stream s))
