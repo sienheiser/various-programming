4635a5a049a7153a7f76aa5fabea4628a31aae38
sienheiser <emmanueldcosta97@gmail.com>
2025-06-12 08:07:53 +0200

    Continued solving

--- TeachYourselfCS/SICP/Chapter4/Chapter4-1-1.scm
+++ TeachYourselfCS/SICP/Chapter4/Chapter4-1-1.scm
@@ -1,32 +1,93 @@
 (define (eval exp env)
-    (cond ((self-evaluating? exp) exp)
-          ((variable? exp) (lookup-variable-value exp env))
-          ((quoted? exp) (text-of-quotation exp))
-          ((assignment? exp) (eval-assignment exp env))
-          ((definition? exp) (eval-definition exp env))
-          ((if? exp) (eval-if exp env))
-          ((lambda? exp) (make-procedure (lambda-parameters exp)
-                                         (lambda-body exp)
-                                         env))
-          ((begin? exp)
-           (eval-sequence (begin-actions exp) env))
-          ((cond? exp) (eval (cond->if exp) env))
-          ((application? exp)
-           (apply (eval (operator exp) env)
-                  (list-of-values (operands exp) env)))
-          (else
-            (error "Unknown expression type: EVAL " exp))))
+  (cond ((self-evaluating? exp) exp)
+        ((variable? exp) (lookup-variable-value exp env))
+        ((quoted? exp) (text-of-quotation exp))
+        ((assignment? exp) (eval-assignment exp env))
+        ((definition? exp) (eval-definition exp env))
+        ((if? exp) (eval-if exp env))
+        ((lambda? exp) (make-procedure (lambda-parameters exp)
+                                       (lambda-body exp)
+                                       env))
+        ((begin? exp)
+         (eval-sequence (begin-actions exp) env))
+        ((cond? exp) (eval (cond->if exp) env))
+        ((application? exp)
+         (apply (eval (operator exp) env)
+                (list-of-values (operands exp) env)))
+        (else
+         (error "Unknown expression type: EVAL" exp))))
+(define (apply procedure arguments)
+  (cond ((primitive-procedure? procedure)
+         (apply-primitive-procedure procedure arguments))
+        ((compound-procedure? procedure)
+         (eval-sequence
+            (procedure-body procedure)
+            (extend-environment
+              (procedure-parameters procedure)
+              arguments
+              (procedure-environment procedure))))
+  (else
+    (error
+     "Unknown procedure type: APPLY" procedure))))
 
-(define (apply procedure arguements)
-    (cond ((primitive-procedure? procedure)
-           (apply-primitive-procedure procedure arguements))
-          ((compound-procedure? procedure)
-           (eval-sequence
-             (procedure-body procedure)
-             (extend-environment
-                (procedure-parameters procedure)
-                arguements
-                (procedure-environment procedure))))
-          (else
-            (error
-              "Unknown procedure type: APPLY" procedure))))
+(define (eval-if exp env)
+  (if (true? (eval (if-predicate exp) env))
+    (eval (if-consequent exp) env)
+    (eval (if-alternative exp) env)))
+
+(define (eval-sequence exps env)
+  (cond ((last-exp? exps)
+         (eval (first-exp exps) env))
+        (else
+         (eval (first-exp exps) env)
+         (eval-sequence (rest-exps exps) env))))
+
+(define (eval-assignment exp env)
+  (set-variable-value! (assignment-variable exp)
+                       (eval (assignment-value exp) env)
+                       env)
+  'ok)
+
+(define (eval-definition exp env)
+  (define-variable! (definition-variable exp)
+                    (eval (definition-value exp) env)
+                    env)
+  'ok)
+
+;-------------------------------------
+;exercise 4.1
+;always left to right
+(define (list-of-values exps env)
+  (if (no-operands? exps)
+    '()
+    (let ((operand (first-operand exps)))
+      (cons (eval operand env)
+            (list-of-values (rest-operands exps) env)))))
+  
+;always right to left
+(define (list-of-valuesV2 exps env)
+  (if (no-operands? exps)
+    '()
+    (let ((rest-values (list-of-valuesV2 (rest-operands exps) env)))
+      (cons (eval (first-operand exps) env)
+            rest-values))))
+;-------------------------------------
+
+(define (self-evaluating? exp)
+  (cond ((number? exp) #t)
+        ((string? exp) #t)
+        (else #f)))
+
+(define (variable? exp) (symbol? exp))
+
+(define (quoted? exp) (tagged-list? exp 'quote))
+(define (text-of-quotation exp) (cadr exp))
+
+(define (tagged-list exp tag)
+  (if (pair? exp)
+    (eq? (car exp) tag)
+    #f))
+
+(define (assignment? exp) (tagged-list? exp 'set!))
+(define (assignment-variable exp) (cadr exp))
+(define (assignment-value exp) (caddr exp)
