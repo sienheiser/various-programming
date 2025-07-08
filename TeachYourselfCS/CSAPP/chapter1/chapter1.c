1d924b8bf3cd7bb183b00820df9ceaef363c3294
sienheiser <emmanueldcosta97@gmail.com>
2025-07-08 07:39:17 +0200

    Started another book

---
+++ TeachYourselfCS/CSAPP/chapter1/chapter1.c
@@ -0,0 +1,16 @@
+// Chapter1.1 Information is bits + context
+// Given a sequence of bytes, the meaning of those bytes change depending on how we decode them
+
+
+// Chapter1.2 Programs are translated by other programs into different forms
+// For a computer to run a source file it must be translated into machine code. What is interesting
+// is that the source file is a text file which is a sequence of bytes; machine code is also a sequence of bytes.
+// To translate the source file into machine code we use a compilation system which is a collection of programs
+//               translation_1           translation_2
+// source_file ----------------> file_1 ----------------> file2 --------------->......
+
+
+// Chapter1.3
+// No useful statements
+
+
