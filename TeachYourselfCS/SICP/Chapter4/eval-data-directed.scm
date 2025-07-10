e2ebd496a9c86e7d576c544ead87873a1ec812ec
sienheiser <emmanueldcosta97@gmail.com>
2025-07-10 18:53:20 +0200

    Added definition in data directed style

--- TeachYourselfCS/SICP/Chapter4/eval-data-directed.scm
+++ TeachYourselfCS/SICP/Chapter4/eval-data-directed.scm
@@ -6,6 +6,14 @@
 (define (get op type)
   (hash-table/get *op-table* (list op type) '()))
 
+;(define *op-table* (make-hash))
+;
+;(define (put op type proc)
+;  (hash-set! *op-table* (list op type) proc))
+;
+;(define (get op type)
+;  (hash-ref *op-table* (list op type) '()))
+
 (define (variable? exp) (symbol? exp))
 (define apply-in-underlying-scheme apply)
 
@@ -55,15 +63,42 @@
         (let ((frame (first-frame env)))
           (scan (frame-variables frame)
                 (frame-values frame)))))
-    (env-loop env))
+    (env-loop env)
+    'set-variable-value!-ok)
 
   
 
   (define (assignment-variable exp) (cadr exp))
   (define (assignment-value exp) (caddr exp))
 
-  (put 'eval 'assignment eval)
+  (put 'eval 'assignment eval-assignemnt)
   'eval-assignment-pkg-ok)
+
+(install-eval-assignment-pkg)
+
+(define (install-eval-definition-pkg)
+  (define (eval-definition exp env)
+    (define-variable! (definition-variable exp)
+                      (eval (definition-value exp) env)
+                      env))
+  (define (definition-variable exp)
+    (cadr exp))
+  (define (definition-value exp)
+    (caddr exp))
+
+  (define (define-variable! variable value env)
+    (let ((frame (first-frame env)))
+      (define (scan vars vals)
+        (cond ((null? vars)
+               (add-binding-to-frame! variable value frame))
+              ((eq? (car vars) variable) (set-car! vals value))
+              (else (scan (cdr vars) (cdr vals)))))
+      (scan (frame-variables frame) (frame-values frame)))
+      'define-variable-ok)
+  (put 'eval 'define eval-definition)
+  'eval-definition-pkg-ok)
+(install-eval-definition-pkg)
+
 
 
 (define the-empty-environment '())
@@ -78,7 +113,12 @@
     null
     (cons (cdar frame) (frame-values (cdr frame)))))
 
-(install-eval-assignment-pkg)
+
+
+
+
+
+
 
 
 (define (enclosing-environment env) (cdr env))
@@ -92,6 +132,10 @@
 (define (add-binding-to-frame! var val frame)
   (set-car! frame (cons var (car frame)))
   (set-cdr! frame (cons val (cdr frame))))
+
+
+(define env (list (make-frame (list 'y) (list 2))))
+(define exp '(define x 4))
 
 
 
