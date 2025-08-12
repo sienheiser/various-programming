e6a1872479ba39d739d8decf7f0051e700d76a08
sienheiser <emmanueldcosta97@gmail.com>
2025-08-12 08:03:31 +0200

    Finished exercies

--- c/the_c_programming_language/Chapter1/chapter1_5_4.c
+++ c/the_c_programming_language/Chapter1/chapter1_5_4.c
@@ -1,31 +1,32 @@
 #include <stdio.h>
+#include <string.h>
 
-#define WORD_COUNTING 1
-#define EX1_11 0
-#define EX1_12 0
+#include <stdio.h>
+#include <string.h>
 
-#if WORD_COUNTING
-    void word_counting();
-#elif EX1_11
-    void ex1_11();
-#elif EX2_11
-    void ex1_12();
-#endif
+void run_function(char *name);
+void word_counting(void);
+void ex1_11(void);
+void ex1_12(void);
 
-int main()
-{
-    if (WORD_COUNTING)
-        word_counting();
-    else if (EX1_11)
-    {
-        ex1_11();  
-    }
-    else if (EX1_12)
-    {
-        ex1_12();
-    }
+int main(void) {
+    char *name = "ex1_12";
+    run_function(name);
     return 0;
 }
+
+void run_function(char *name) {
+    if (strcmp(name, "word_counting") == 0) {
+        word_counting();
+    } else if (strcmp(name, "ex1_11") == 0) {
+        ex1_11();
+    } else if (strcmp(name, "ex1_12") == 0) {
+        ex1_12();
+    } else {
+        printf("Unknown function name: %s\n", name);
+    }
+}
+
 
 void word_counting(){
     #define IN 1
@@ -46,5 +47,34 @@ void word_counting(){
         }
     }
     printf("%d %d %d\n",nl,nw,nc);
+}
+
+void ex1_11(){
+   /*Q:How would you test the word count program? What kinds of input are most likely to uncover bugs if there are any?
+     A: Make a text file with a known number of lines, words and charaters and check whether the word counting program
+     counts these parameters correctly. The most likely bugs will occur from escape characters like the arrow keys.*/ 
+   
+}
+
+void ex1_12(){
+    /*Q:Write a program that prints its input one word per line.*/
+    #define IN 1
+    #define OUT 0
+
+    int c, state;
+    state = OUT;
+    while ((c = getchar()) != EOF){
+        if (c == ' ' || c == '\n' || c == '\t'){
+            putchar('\n');
+            state = OUT;
+        }
+        else if (state == OUT) {
+            state = IN;
+            putchar(c);
+        } else {
+            putchar(c);
+        }
+    }
+
 }
 
