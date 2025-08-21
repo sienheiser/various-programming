05ad9419289d0e29f027aa6543dfda991086e787
sienheiser <emmanueldcosta97@gmail.com>
2025-08-21 08:25:33 +0200

    continued solving

--- c/the_c_programming_language/Chapter1/chapter1_9_0/ex_1_16.c
+++ c/the_c_programming_language/Chapter1/chapter1_9_0/ex_1_16.c
@@ -1,35 +1,43 @@
 #include <stdio.h>
-#define MAXLINE 1000
+#define MAXLINE 6
 
-int getline(char line[],int len);
+int get_line(char line[],int len);
 void copy(char to[], char from[]);
 
 int main(){
     char line[MAXLINE],longest[MAXLINE];
     int len = 0;
     int max = 0;
-    while ((len = getline(line,MAXLINE))>0){
+    while ((len = get_line(line,MAXLINE))>0){
         if (len > max){
             max = len;
             copy(longest,line);
         }
     }
     if (max > 0)
+        printf("length %d\n",max);
         printf("%s",longest);
     return 0;
 }
 
-int getline(char line[],int len){
+int get_line(char line[],int len){
     int i = 0;
     int c = 0;
-    for (i=0;i<len-1 && (c=getchar())!=EOF && c != '\n';++i){
-        line[i] = c;
-    }
-    if (c == '\n'){
-        line[i] = c;
+    while ((c = getchar()) != EOF){
         ++i;
+        if (i < len - 1){
+            line[i] = c;
+
+            if (c == '\n'){
+                line[i] = c; 
+                ++i;
+                line[i] = '\0';
+            }
+        }
+        else if (i == len - 1){
+            line[i] = '\0';
+        }
     }
-    line[i] = '\0';
 
     return i;
 }
