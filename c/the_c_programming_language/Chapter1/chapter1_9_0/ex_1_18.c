8d41efb8812e14f66fb50fae8c3c96037addbcfd
sienheiser <emmanueldcosta97@gmail.com>
2025-08-22 07:59:24 +0200

    Solved exercise

--- c/the_c_programming_language/Chapter1/chapter1_9_0/ex_1_18.c
+++ c/the_c_programming_language/Chapter1/chapter1_9_0/ex_1_18.c
@@ -2,7 +2,10 @@
 #define MAXLINE 10
 
 int get_line(char line[],int len);
-void remove_blanks(char line[],int len);
+void remove_blanks(char line[], int len);
+int has_trailing_blank(char line[],int len);
+int blank_line(char line[], int len);
+
 int main()
 {
     char line[MAXLINE];
@@ -10,9 +13,12 @@ int main()
 
     while ((len = get_line(line,MAXLINE)) != 0)
     {
-        remove_blanks(line);
+        remove_blanks(line,len);
 
-        printf("%s",line);
+        if (blank_line(line,len) != 1)
+        { 
+            printf("%s",line);
+        }
     }
     return 0;
 }
@@ -36,8 +42,24 @@ int get_line(char line[], int len)
 
 void remove_blanks(char line[],int len)
 {
-    for (len; len == 0; --len)
+    char c;
+    if (has_trailing_blank(line, len))
     {
+        for (len = len-2; len >= 0 && (line[len] == ' ' || line[len] == '\t'); --len)
+        {
 
-    }
+            line[len] = '\0';
+        }
+        line[len+1] = '\n';
+    } 
+}
+
+int has_trailing_blank(char line[],int len)
+{
+    return line[len-3] == ' ' || line[len-3] == '\t';
+}
+
+int blank_line(char line[], int len)
+{
+    return len == 1;
 }
