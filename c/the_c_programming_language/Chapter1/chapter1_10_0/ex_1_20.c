228e4d4a1ad30e3ce07596d8c97aeabd2660eedd
sienheiser <emmanueldcosta97@gmail.com>
2025-09-02 09:08:09 +0200

    Kinda solved the problem

--- c/the_c_programming_language/Chapter1/chapter1_10_0/ex_1_20.c
+++ c/the_c_programming_language/Chapter1/chapter1_10_0/ex_1_20.c
@@ -1,26 +1,55 @@
 #include "utilities.h"
+#include <stdio.h>
 #define MAXLINE 10
-void detab(char line[],int len);
+#define TAB_STOP_GORUP_LEN
+#define TAB_STOP_APPEARANCE
+int get_line(char line[],int len);
+void detab(char line[],int len, char newline[]);
 int main()
 {
     int len = 0;
     char line[MAXLINE];
+    char newline[MAXLINE];
     while ((len = get_line(line,MAXLINE)) != 0)
     {
-        detab(line,len);
-        printf("%s",line);
+        detab(line,len,newline);
+        printf("%s",newline);
     }
     return 0;
 }
 
-void detab(char line[], int len)
+int get_line(char line[],int len)
 {
     int i;
+    char c;
+
+    for (i=0; (c = getchar()) != EOF && c != '\n' && i < len-1; ++i)
+    {
+        line[i] = c;
+    }
+    if (c == '\n')
+    {
+        line[i] = '\n';
+        ++i;
+    }
+    line[i] = '\0';
+    return i;
+}
+
+void detab(char line[], int len, char newline[])
+{
+    int i,j;
+    j = 0;
     for (i = 0; i < len; ++i)
     {
-        if (line[i] = '\t')
+        if (line[i] == '\t')
         {
-            line[i] = '  ';
+            newline[i+j] = ' ';
+            ++j;
+            newline[i+j] = ' ';
+        } else 
+        {
+            newline[i+j] = line[i];
         }
     }
 }
