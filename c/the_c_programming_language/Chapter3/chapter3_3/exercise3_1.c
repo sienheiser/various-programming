cb8d99a8e05866dd967f64c5b22ede3e52ba87bd
sienheiser <emmanueldcosta97@gmail.com>
2025-09-16 07:12:49 +0200

    Found out that both codes do not work

--- c/the_c_programming_language/Chapter3/chapter3_3/exercise3_1.c
+++ c/the_c_programming_language/Chapter3/chapter3_3/exercise3_1.c
@@ -3,28 +3,32 @@
 
 
 int binsearch(int x, int v[], int n);
+int binsearch2(int x, int v[], int n);
 int main()
 {
     int v[10] = {1,3,5,7,9,11,13,15,17,19};
+
+    int n = binsearch2(19,v,10);
+
+    printf("%d\n",n);
     return 0;
 }
 
 int binsearch(int x, int v[], int n)
 {
-    int low,mid,high;
+    int low, high, mid;
     low = 0;
-    high = n-1;
-    while (low <= high)
-    {
-        mid = (high + low)/2;
+    high = n - 1;
+    while (low <= high) {
+        mid = (low+high)/2;
         if (x < v[mid])
             high = mid + 1;
-        else if (v[mid] < x)
+        else if (x > v[mid])
             low = mid + 1;
-        else
+        else 
             return mid;
     }
-    return -1;
+    return -1; 
 }
 
 int binsearch2(int x, int v[], int n)
@@ -37,6 +41,7 @@ int binsearch2(int x, int v[], int n)
     mid = (low+high)/2;
     while (x < v[mid] ? (high = mid + 1) : (low = mid) && (low <= high))
     {
+        printf("%d\n",high);
         if (v[mid] == x)
             return mid;
         mid = (low+high)/2;
