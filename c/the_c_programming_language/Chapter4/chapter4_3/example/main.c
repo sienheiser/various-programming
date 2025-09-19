0c6a14055a7d57fa5f0e1d429cd76569d398429e
sienheiser <emmanueldcosta97@gmail.com>
2025-09-19 08:43:50 +0200

    Wrote the code from scratch

--- c/the_c_programming_language/Chapter4/chapter4_3/example/main.c
+++ c/the_c_programming_language/Chapter4/chapter4_3/example/main.c
@@ -2,21 +2,22 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-#define MAXOP 100
+#define MAXOP 100 //Max size of operand. If n then can only handle n digit long numbers
 #define NUMBER '0'
-int getop(char []);
-void push(double);
+
+
+char getop(char s[]);
+void push(double f);
 double pop(void);
 
 int main()
 {
-    int type;
+    char type,s[MAXOP];
     double op2;
-    char s[MAXOP];
+    
     while((type = getop(s)) != EOF)
     {
-        switch (type)
-        {
+        switch (type) {
             case NUMBER:
                 push(atof(s));
                 break;
@@ -32,83 +33,86 @@ int main()
                 break;
             case '/':
                 op2 = pop();
-                if (op2 != '0')
+                if (op2 != 0){
                     push(pop()/op2);
-                else
-                    printf("error: zero divisor\n");
+                } else {
+                    printf("Error divisor is 0");
+                }
                 break;
             case '\n':
-                printf("\t%.8g\n",pop());
+                printf("\t%.8g\n", pop());
                 break;
             default:
-                printf("error: unknown command %s\n",s);
+                printf("error: unknown command %s\n", s);
                 break;
         }
     }
-
     return 0;
 }
 
-#define MAXVAL 100
 
+#define STACKSIZE 100
+double val[STACKSIZE];
 int sp = 0;
-double val[MAXVAL];
 
 void push(double f)
 {
-    if (sp < MAXVAL)
+    if (sp < STACKSIZE)
         val[sp++] = f;
-    else
-        printf("error: stack full, can't push %g\n",f);
+    else 
+        printf("push: Stack is full cannot push %g\n",f);
+    
 }
 
 double pop(void)
 {
     if (sp > 0)
         return val[--sp];
-    else{
-        printf("error: stack empty\n");
+    else {
+        printf("pop: Stack is empty");
         return 0.0;
     }
 }
 
-
-int getch(void);
-void ungetch(int);
-/* getop: get next character or numeric operand */
-int getop(char s[])
+char getch(void);
+void ungetch(char);
+char getop(char s[])
 {
-    int i, c;
+    char c;
     while ((s[0] = c = getch()) == ' ' || c == '\t')
-    ;
+        ;
     s[1] = '\0';
+
     if (!isdigit(c) && c != '.')
-        return c; /* not a number */
-    i = 0;
-    if (isdigit(c)) /* collect integer part */
+        return c;
+
+    int i = 0;
+    if (isdigit(c))
         while (isdigit(s[++i] = c = getch()))
             ;
-    if (c == '.') /* collect fraction part */
+    if (c == '.')
         while (isdigit(s[++i] = c = getch()))
             ;
     s[i] = '\0';
-        if (c != EOF)
-    ungetch(c);
+    if (c != EOF)
+        ungetch(c);
     return NUMBER;
 }
 
 
-#define BUFSIZE 100
-char buf[BUFSIZE]; /* buffer for ungetch */
-int bufp = 0; /* next free position in buf */
-int getch(void) /* get a (possibly pushed-back) character */
+#define BUFFSIZE 100
+char buff[BUFFSIZE];
+int buffp;
+
+char getch(void)
 {
-    return (bufp > 0) ? buf[--bufp] : getchar();
+    return (buffp>0) ? buff[--buffp] : getchar();
 }
-void ungetch(int c) /* push character back on input */
+
+void ungetch(char c)
 {
-    if (bufp >= BUFSIZE)
+    if (buffp >= BUFFSIZE)
         printf("ungetch: too many characters\n");
     else
-        buf[bufp++] = c;
+        buff[buffp++] = c;
 }
